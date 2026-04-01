69cd26d795c266d0738c73c55be208b88fcf7dc0
Monish Mahesh Gori <mmg@Monishs-MacBook-Air.local>
2026-04-01 16:39:08 +0530

    content: move festival video to the first position

--- src/data/videos.js
+++ src/data/videos.js
@@ -1,10 +1,19 @@
 export const videos = [
   {
+    id: 13,
+    title: "Shri Sodevpir Dada Annual Festival - 31.03.2026",
+    gujarati: "શ્રી સોદેવપીર દાદા નો વાર્ષિક તિથી મહોત્સવ તા : ૩૧.૦૩.૨૦૨૬",
+    hindi: "श्री सोदेवपीर दादा वार्षिक तिथि महोत्सव दि: ३१.०३.२०२६",
+    english: "Shri Sodevpir Dada Varshik Tithi Mahotsav - 31.03.2026",
+    youtubeId: "mqw7P4snFvs",
+    thumbnail: "https://img.youtube.com/vi/mqw7P4snFvs/mqdefault.jpg"
+  },
+  {
     id: 1,
     title: "Shree Sodevpeer Dada Bhajan Santwani 2025",
     gujarati: "શ્રી સોદેવપીર દાદા ભજન સંતવાણી ૨૦૨૫",
     hindi: "श्री सोदेवपीर दादा भजन संतवाणी २०२५",
-            english: "Shri sodevapira dada bhajana samtavani 2025",
+    english: "Shri sodevapira dada bhajana samtavani 2025",
     youtubeId: "GOW7QiWAk2M",
     thumbnail: "https://img.youtube.com/vi/GOW7QiWAk2M/mqdefault.jpg"
   },
@@ -13,7 +22,7 @@ export const videos = [
     title: "Shree Sodevpeer Dada Santwani 03",
     gujarati: "શ્રી સોદેવપીર દાદા સંતવાણી ૦૩",
     hindi: "श्री सोदेवपीर दादा संतवाणी ०३",
-            english: "Shri sodevapira dada samtavani 03",
+    english: "Shri sodevapira dada samtavani 03",
     youtubeId: "jyGbXanNikk",
     thumbnail: "https://img.youtube.com/vi/jyGbXanNikk/mqdefault.jpg"
   },
@@ -22,7 +31,7 @@ export const videos = [
     title: "Shree Sodevpeer Dada Santwani 02",
     gujarati: "શ્રી સોદેવપીર દાદા સંતવાણી ૦૨",
     hindi: "श्री सोदेवपीर दादा संतवाणी ०૨",
-            english: "Shri sodevapira dada samtavani 0૨",
+    english: "Shri sodevapira dada samtavani 0૨",
     youtubeId: "JiIv5wDs3UI",
     thumbnail: "https://img.youtube.com/vi/JiIv5wDs3UI/mqdefault.jpg"
   },
@@ -30,8 +39,8 @@ export const videos = [
     id: 4,
     title: "Shree Sodevpeer Dada Santwani 04",
     gujarati: "શ્રી સોદેવપીર દાદા સંતવાણી ૦૪",
-    hindi: "श्री सोदेवपीर दादा संतवाणी ०૪",
-            english: "Shri sodevapira dada samtavani 0૪",
+    hindi: "श्री सोदेवपीર दादा संतवाणी ०૪",
+    english: "Shri sodevapiર dada samtavani 0૪",
     youtubeId: "wVL7xzrfeWo",
     thumbnail: "https://img.youtube.com/vi/wVL7xzrfeWo/mqdefault.jpg"
   },
@@ -39,8 +48,8 @@ export const videos = [
     id: 5,
     title: "Shree Sodevpeer Dada Kori Juhu 03",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી જુહુ ૦૩",
-    hindi: "श्री सोदेवपीर दादा कोरी जुहू ०३",
-            english: "Shri sodevapira dada kori juhu 03",
+    hindi: "श्री सोदेवपीર दादा कोरी जुहू ०३",
+    english: "Shri sodevapiર dada kori juhu 03",
     youtubeId: "Yj8efoeil3I",
     thumbnail: "https://img.youtube.com/vi/Yj8efoeil3I/mqdefault.jpg"
   },
@@ -48,8 +57,8 @@ export const videos = [
     id: 6,
     title: "Shree Sodevpeer Dada Kori Juhu 02",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી જુહુ ૦૨",
-    hindi: "श्री सोदेवपीर दादा कोरी जुहू ०૨",
-            english: "Shri sodevapira dada kori juhu 0૨",
+    hindi: "श्री सोदेवपीર दादा कोरी जुहू ०૨",
+    english: "Shri sodevapiર dada kori juhu 0૨",
     youtubeId: "rPF1PsdVX2E",
     thumbnail: "https://img.youtube.com/vi/rPF1PsdVX2E/mqdefault.jpg"
   },
@@ -57,8 +66,8 @@ export const videos = [
     id: 7,
     title: "Shree Sodevpeer Dada Kori Juhu 04",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી જુહુ ૦૪",
-    hindi: "श्री सोदेवपीર दादा कोरी जुहू ०૪",
-            english: "Shri sodevapiર dada kori juhu 0૪",
+    hindi: "श्री सोदेवપીર दादा कोरी जुहू ०૪",
+    english: "Shri sodevapiર dada kori juhu 0૪",
     youtubeId: "83Y5G3NYOpU",
     thumbnail: "https://img.youtube.com/vi/83Y5G3NYOpU/mqdefault.jpg"
   },
@@ -66,8 +75,8 @@ export const videos = [
     id: 8,
     title: "Shree Sodevpir Dada Bhajan - Jigar Dama",
     gujarati: "શ્રી સોદેવપીર દાદા ભજન - જીગર દામા",
-    hindi: "श्री सोदेवपीर दादा भजन - जिगर दामा",
-            english: "Shri sodevapira dada bhajana - jigara dama",
+    hindi: "श्री सोदेवपीર दादा भजन - जिगर दामा",
+    english: "Shri sodevapiર dada bhajana - jigara dama",
     youtubeId: "l2VPfJg0gAs",
     thumbnail: "https://img.youtube.com/vi/l2VPfJg0gAs/mqdefault.jpg"
   },
@@ -75,8 +84,8 @@ export const videos = [
     id: 9,
     title: "Shree Sodevpir Dada Kori",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી",
-    hindi: "श्री सोदेवपीर दादा कोरी",
-            english: "Shri sodevapira dada kori",
+    hindi: "श्री सोदेवપીર दादा कोरी",
+    english: "Shri sodevapiર dada kori",
     youtubeId: "iHm1lh-H1JM",
     thumbnail: "https://img.youtube.com/vi/iHm1lh-H1JM/mqdefault.jpg"
   },
@@ -84,18 +93,17 @@ export const videos = [
     id: 10,
     title: "Shree Sodevpir Dada Na Bhajan - Jigar Dama",
     gujarati: "શ્રી સોદેવપીર દાદા ના ભજન - જીગર દામા",
-    hindi: "श्री सोदेवपीर दादा के भजन - जिगर दामा",
-            english: "Shri sodevapira dada ke bhajana - jigara dama",
+    hindi: "श्री सोदेवपीર दादा के भजन - जिगर दामा",
+    english: "Shri sodevapiર dada ke bhajana - jigara dama",
     youtubeId: "SZRCJFBEEcA",
     thumbnail: "https://img.youtube.com/vi/SZRCJFBEEcA/mqdefault.jpg"
   },
-
   {
     id: 11,
     title: "Shree Sodevpir Dada Kori 3",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી ૩",
-    hindi: "श्री सोदेवपीर दादा कोरी ३",
-            english: "Shri sodevapira dada kori 3",
+    hindi: "श्री सोदेवપીર दादा कोरी ३",
+    english: "Shri sodevapiર dada kori 3",
     youtubeId: "QQX7Y6zUBIU",
     thumbnail: "https://img.youtube.com/vi/QQX7Y6zUBIU/mqdefault.jpg"
   },
@@ -103,18 +111,9 @@ export const videos = [
     id: 12,
     title: "Shree Sodevpir Dada Kori 2",
     gujarati: "શ્રી સોદેવપીર દાદા કોરી ૨",
-    hindi: "श्री सोदेवपीर दादा कोरी ૨",
-            english: "Shri sodevapira dada kori ૨",
+    hindi: "श्री सोदेवપીર दादा कोरी ૨",
+    english: "Shri sodevapiર dada kori ૨",
     youtubeId: "9psE9oAjraE",
     thumbnail: "https://img.youtube.com/vi/9psE9oAjraE/mqdefault.jpg"
-  },
-  {
-    id: 13,
-    title: "Shri Sodevpir Dada Annual Festival - 31.03.2026",
-    gujarati: "શ્રી સોદેવપીર દાદા નો વાર્ષિક તિથી મહોત્સવ તા : ૩૧.૦૩.૨૦૨૬",
-    hindi: "श्री सोदेवपीर दादा वार्षिक तिथि महोत्सव दि: ३१.०३.२०२६",
-    english: "Shri Sodevpir Dada Varshik Tithi Mahotsav - 31.03.2026",
-    youtubeId: "mqw7P4snFvs",
-    thumbnail: "https://img.youtube.com/vi/mqw7P4snFvs/mqdefault.jpg"
   }
 ];
